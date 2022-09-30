092fdb8a672c1700c8fb87027e73e5b122994c14
feezotter <julianazotter@proton.me>
2022-09-30 15:06:15 +0200

    there is a connection

--- index.php
+++ index.php
@@ -4,31 +4,51 @@
     
 
     <head>
-        <title>Aktuelle Artikel</title>
+        <title>Food</title>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     </head>
       
-    <body>
-        <header>
-            <img src="/Bilder/Banner/Start-Banner.jpg" alt="Banner" title="Homepage-Webhilfe" />
-        </header>
-          
+    <body>       
+        
+        <?php
+            $servername = "localhost";
+            $username = "testUser";
+            $password = "123456";
+            $dbname = "test";
+            $tableContent = "";
+            $tableContentArray = array();
+
+            // Create connection
+            $conn = new mysqli($servername, $username, $password, $dbname);
+            // Check connection
+            if ($conn->connect_error) {
+                die("Connection failed: " . $conn->connect_error);
+            }
+
+            $sql = "SELECT ID_testFood, food, rating FROM testfood";
+            $result = $conn->query($sql);
+
+            if ($result->num_rows > 0) {
+            // output data of each row
+            while($row = $result->fetch_assoc()) {
+                $tableContent = $tableContent. 
+                "<tr>"
+                    ."<td>".$row["ID_testFood"]."</td>"
+                    ."<td>".$row["food"]."</td>"
+                    ."<td>".$row["rating"]."</td>"
+                ."</tr>";
+            }
+            } else {
+                $tableContent = "0 results";
+            }
+            $conn->close();
+        ?>
+
+
         <nav> 
-            
             <input type="text" id="searchbar" name="searchbar" tabindex="1" rows="1" minlength="2" autofocus/>
             <input type="submit" value="🔍"/>
         </nav>
-
-        
-        <main>
-            <div id="zeit">
-                <?php // Ausgabe der aktuellen Serverzeit ?>
-            </div>
-            <p>...</p>
-            <?php
-            // Laden der aktuellen Artikel aus einer Datenbank und anzeigen
-            ?>
-        </main>
           
         <table class="table table-hover">
             <thead id="tabletop">
@@ -39,36 +59,7 @@
                 </tr>
             </thead>
             <tbody id="tableContent">
-                <?php
-                    $servername = "localhost";
-                    $username = "testUser";
-                    $password = "123456";
-                    $dbname = "test";
-
-                    // Create connection
-                    $conn = new mysqli($servername, $username, $password, $dbname);
-                    // Check connection
-                    if ($conn->connect_error) {
-                    die("Connection failed: " . $conn->connect_error);
-                    }
-
-                    $sql = "SELECT ID_testFood, food, rating FROM testfood";
-                    $result = $conn->query($sql);
-
-                    if ($result->num_rows > 0) {
-                    // output data of each row
-                    while($row = $result->fetch_assoc()) {
-                        echo "<tr>";
-                        echo "<td>".$row["ID_testFood"]."</td>";
-                        echo "<td>".$row["food"]."</td>";
-                        echo "<td>".$row["rating"]."</td>";
-                        echo "</tr>";
-                    }
-                    } else {
-                    echo "0 results";
-                    }
-                    $conn->close();
-                ?>
+                <?=$tableContent?>
             </tbody>
         </table>
 
