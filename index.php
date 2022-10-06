5042ee56331f14c709c56ca4f49d0ca1ce2f8ec0
feezotter <julianazotter@proton.me>
2022-10-06 13:04:28 +0200

    extra file for foodList

--- index.php
+++ index.php
@@ -10,37 +10,7 @@
       
     <body>        
         <?php
-            $servername = "localhost";
-            $username = "testUser";
-            $password = "123456";
-            $dbname = "test";
-            $tableContent = "";
-            $tableContentArray = array();
-
-            // Create connection
-            $conn = new mysqli($servername, $username, $password, $dbname);
-            // Check connection
-            if ($conn->connect_error) {
-                die("Connection failed: " . $conn->connect_error);
-            }
-
-            $sql = "SELECT ID_testFood, food, rating FROM testfood";
-            $result = $conn->query($sql);
-
-            if ($result->num_rows > 0) {
-            // output data of each row
-            while($row = $result->fetch_assoc()) {
-                $tableContent = $tableContent. 
-                "<tr>"
-                    ."<td>".$row["ID_testFood"]."</td>"
-                    ."<td><code>".$row["food"]."</code></td>"
-                    ."<td>".$row["rating"]."</td>"
-                ."</tr>";
-            }
-            } else {
-                $tableContent = "0 results";
-            }
-            $conn->close();
+            include("foodTable.php");
         ?>
 
         <nav> 
