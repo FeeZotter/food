a2a8e2e2f04770ba661fc4d2c90a30a64bfe57f0
feezotter <julianazotter@proton.me>
2022-10-07 14:44:49 +0200

    search by rating and by value

--- index.php
+++ index.php
@@ -2,10 +2,10 @@
   
 <html>
     <head>
-        <title>Food</title>
-        <link rel="stylesheet" href="index.css">
+        <title>Foood</title>
+        <link rel="stylesheet" href="./style/style.css">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-        <script src="./index.js"></script>  
+        <script src="./js/index.js"></script>  
     </head>
       
     <body>        
@@ -14,18 +14,16 @@
         ?>
 
         <form action="insertFood.php" method="post">
-            
             <label for="food">Food:</label>
             <input type="text" name="food" id="food">
-
             <label for="lastName">Rating:</label>
             <input type="number" inputmode="numeric" name="rating" id="rating" value="0" min="0" max="10">
-
             <input type="submit" value="Submit">
         </form>
 
         <nav> 
-            <input class="wideInput" type="text" id="searchbar" name="searchbar" tabindex="1" rows="1" minlength="2" autofocus onkeyup="search()"/>
+            <input class="wideInput" type="text" id="sortValue" name="searchbar" tabindex="1" rows="1" minlength="2" autofocus onkeyup="searchByName()"/>
+            <input class="tightInput" type="text" id="sortRating" name="searchbar" tabindex="1" rows="1" minlength="2" autofocus onkeyup="searchByRating()"/>
         </nav>
           
         <table class="table table-hover" id="foodTable">
---
+++ js/index.js
@@ -0,0 +1,37 @@
+var filter, table, tr, td, i, txtValue;
+
+function searchByName() {
+    filter = document.getElementById("sortValue").value.toUpperCase();
+    table = document.getElementById("foodTable");
+    tr = table.getElementsByTagName("tr");
+    // Loop through all table rows, and hide those who don't match the search query
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[0];
+        if (td) {
+            txtValue = td.textContent || td.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+        }
+    }
+}
+
+function searchByRating() {
+    filter = document.getElementById("sortValue").value.toUpperCase();
+    table = document.getElementById("foodTable");
+    tr = table.getElementsByTagName("tr");
+    // Loop through all table rows, and hide those who don't match the search query
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[1];
+        if (td) {
+            txtValue = td.textContent || td.innerText;
+            if (txtValue.toUpperCase() == filter || filter == '') {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+        }
+    }
+}
