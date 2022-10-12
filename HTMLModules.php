166cb15a383f76f7ba18aeab4d91349d9a5c4548
FeeZotter <90189171+FeeZotter@users.noreply.github.com>
2022-10-12 11:31:05 +0200

    added returntable

--- HTMLModules.php
+++ HTMLModules.php
@@ -3,25 +3,26 @@
     {
         function contentTable($dbconn, $table)
         {
+            $returnTable = "";
             $contentTableResult = $dbconn->query("SELECT * FROM " . $table);
             if ($contentTableResult->num_rows > 0) 
             {
                 // output data of each row
                 while($row = $contentTableResult->fetch_assoc()) 
                 {
-                    $contentTable = $contentTable. 
+                    $returnTable = $returnTable . 
                     "<tr 
-                        class='color" . $row[$this->tableRating]  . "'>"
-                        . "<td><b>"   . $row[$this->tableContent] . "</b</td>"
-                        . "<td><b>"   . $row[$this->tableRating]  . "</b></td>"
+                        class='color" . $row[tableRating]  . "'>"
+                        .   "<td><b>" . $row[$this->tableContent] . "</b</td>"
+                        .   "<td><b>" . $row[$this->tableRating]  . "</b></td>"
                     ."</tr>";
                 }
             } 
             else 
             {
-                $contentTable = "0 results";
+                $returnTable = "0 results";
             }
-            return $contentTable;
+            return $returnTable;
         }   
     }
 ?>
