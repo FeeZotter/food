560797cf646e6e86268a6fa47d7cbc8032630d2d
FeeZotter <90189171+FeeZotter@users.noreply.github.com>
2022-10-16 12:05:07 +0200

    created preferencesDB.php

--- preferencesDB.php
+++ preferencesDB.php
@@ -1,5 +1,6 @@
 <?php
     $table                   = "preferences";
+    $tables                  = array("persons", "categories", "cross_person_categories", "preferences");
     $persons                 = array("name", "password", "alias");
     $categories              = array("category");
     $cross_person_categories = array("cross_person_categories_id", "persons_id", "categories_id");
