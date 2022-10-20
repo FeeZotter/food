83b60d489a926602eb7f10fb24398c43619dcc37
FeeZotter <90189171+FeeZotter@users.noreply.github.com>
2022-10-20 12:22:46 +0200

    view2Test.php works as expected

--- view2Test.php
+++ view2Test.php
@@ -16,25 +16,23 @@
                 $returnTable = "";
 
                 $alias = mysqli_real_escape_string($dbconn, $_REQUEST['alias']);
-                $sql = "SELECT name WHERE alias=Fee FROM persons";
-                $result = mysqli_query($dbconn, $sql);
-                $sql = "SELECT categories_id WHERE persons_id=$result 
-                        FROM cross_person_categories";
+                $sql = "SELECT name FROM persons WHERE alias='$alias'";
+                $result = mysqli_fetch_assoc(mysqli_query($dbconn, $sql));
+                $sql = "SELECT categories_id FROM cross_person_categories WHERE persons_id='" . $result['name'] . "'";
                 $result = mysqli_query($dbconn, $sql);
                 
                 while($row = mysqli_fetch_assoc($result))
                 {
                     $returnTable .=
                     "<tr>"
-                        .   "<td><b>" . $row["alias"]          . "</b</td>"
-                        .   "<td><b>" . $row["categories_id"]  . "</b></td>"
+                        .   "<td><b>" . $row['categories_id'] . "</b</td>"
                     ."</tr>";
                 }
                 
                 return '<table class="table table-hover" id="foodTable">
                             <thead id="tabletop">
                                 <tr>
-                                    <th scope="col">food</th> <th scope="col">rating</th>
+                                    <th scope="col">' . $alias . '</th>
                                 </tr>
                             </thead>
                             <tbody id="tableContent">' .
