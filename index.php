2b6a7bdb450579d1c4fac28de72bdd7d5bd7de0b
FeeZotter <90189171+FeeZotter@users.noreply.github.com>
2022-10-20 13:42:20 +0200

    vielleicht nutze ich Routing

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Likeings</title>
+        <link rel="stylesheet" href="./style/style.css">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        <script src="./js/index.js"></script>  
+    </head>
+    <body>
+        <h1>Likeings</h1>
+        <p>Drücke auf <a href="ViewLayer1.php"> START </a> um dir anzusehen was andere mögen</p>
+
+    </body>
+</html>
