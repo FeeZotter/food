e6fd1b022cd77ccb4d2c0d5b23d8c717b7798c78
FeeZotter <90189171+FeeZotter@users.noreply.github.com>
2022-10-20 17:24:28 +0200

    asdf

--- ViewLayer1.php
+++ ViewLayer1.php
@@ -14,7 +14,6 @@
         <title>title</title>
         <link rel="stylesheet" href="./style/style.css">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-        <script src="./js/index.js"></script>  
     </head>
     <body>
         <form action="ViewLayer2.php" method="post">
@@ -25,3 +24,5 @@
         <?php echo $htmlComp->table($dbconn, 'alias', 'persons'); ?>
     </body>
 </html>
+<script src="./js/index.js"></script>  
+<script src="./js/ViewLayer1.js"></script> 
---
+++ js/ViewLayer1.js
@@ -0,0 +1,4 @@
+var table = document.getElementById('table');
+table.onclick = () => {
+    console.log(event.target)
+}
