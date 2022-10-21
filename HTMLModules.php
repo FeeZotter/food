1c3cd635e7cb4b59d187bffedc2e76cd9117ee6a
FeeZotter <90189171+FeeZotter@users.noreply.github.com>
2022-10-21 09:50:06 +0200

    ViewLayer2 frontent works

--- HTMLModules.php
+++ HTMLModules.php
@@ -25,6 +25,32 @@
                         '</tbody>
                     </table>';
         }
+
+        function tableWhere($dbconn, $select, $from, $where)
+        {
+            $dml = new DMLModules();
+            $array = $dml->getTableWhere($dbconn, $select, $from, $where);
+            $returnTable = "";
+            foreach ($array as $value)
+            {
+                $returnTable .=
+                "<tr>"
+                .   "<td>{$value}</td>"
+                ."</tr>";
+            }
+
+            return '<table class="table table-hover" id="table">
+                        <thead id="tabletop">
+                            <tr>
+                                <th scope="col">' . $select . '</th>
+                            </tr>
+                        </thead>
+                        <tbody id="tableContent">' .
+                            $returnTable .
+                        '</tbody>
+                    </table>';
+        }
+
         public function table2($dbconn, $select, $select2, $from)
         {
             $dml = new DMLModules();
@@ -52,7 +78,7 @@
                     </table>';
         }
 
-        function insertFood()
+        function insert()
         {
 
         }
--- ViewLayer2.php
+++ ViewLayer2.php
@@ -3,45 +3,29 @@
     <head>
         <?php
             include("HTMLModules.php");
-            include("DMLModules.php");
             include("DBConnection.php");
             include("preferencesDB.php");
             include("serverconfig.php");
-            $htmlComp = new HTMLModules();
-            $db = new DBConnection($servername, $username, $password, $dbname);
+            include("DMLModules.php");
+            $db = new DBConnection($servername, 
+                                   $username, 
+                                   $password, 
+                                   $dbname);
             $dbconn = $db->getConnection();
-
-    
             function layer2Table($dbconn)
             {
                 $dml = new DMLModules();
-//$sql = "SELECT name FROM persons WHERE alias='$alias'";
-                $returnTable = "";
-
+                $htmlComp = new HTMLModules();
                 $alias = mysqli_real_escape_string($dbconn, $_REQUEST['alias']);
-                $result = $dml->getTableWhere($dbconn, 'name', "persons", "alias='$alias'");
-                $sql = "SELECT categories_id FROM cross_person_categories WHERE persons_id='" . $result['name'] . "'";
-
-                $result = mysqli_query($dbconn, $sql);
-                
-                while($row = mysqli_fetch_assoc($result))
-                {
-                    $returnTable .=
-                    "<tr>"
-                        .   "<td><b>" . $row['categories_id'] . "</b</td>"
-                    ."</tr>";
-                }
-                
-                return '<table class="table table-hover" id="foodTable">
-                            <thead id="tabletop">
-                                <tr>
-                                    <th scope="col">' . $alias . '</th>
-                                </tr>
-                            </thead>
-                            <tbody id="tableContent">' .
-                                $returnTable .
-                            '</tbody>
-                        </table>';
+                $result = $dml->getTableWhere($dbconn, 
+                                              'name', 
+                                              "persons", 
+                                              "alias='$alias'");
+                $tableContent = $htmlComp->tableWhere($dbconn, 
+                                                      'categories_id', 
+                                                      "cross_person_categories", 
+                                                      "persons_id='$result[0]'");
+                return $tableContent;
             }
         ?>
         <title>title</title>
