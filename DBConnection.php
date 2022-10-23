eb3e4163e3e3f6e0f744a3196cb3a414bc9a2826
FeeZotter <90189171+FeeZotter@users.noreply.github.com>
2022-10-23 19:44:23 +0200

    refactored DBConnection

--- DBConnection.php
+++ DBConnection.php
@@ -1,23 +1,14 @@
 <?php
     class DBConnection
     {
-        private $servername;
-        private $username;
-        private $password;
-        private $dbname;
         private $conn;
 
         function __construct($servername, $username, $password, $dbname)
         {
-            $this->servername = $servername;
-            $this->username   = $username;
-            $this->password   = $password;
-            $this->dbname     = $dbname;
- 
-            $this->conn = new mysqli($this->servername, 
-                                     $this->username, 
-                                     $this->password, 
-                                     $this->dbname);
+            $this->conn = new mysqli($servername, 
+                                     $username, 
+                                     $password, 
+                                     $dbname);
 
             if ($this->conn->connect_error) 
             {
