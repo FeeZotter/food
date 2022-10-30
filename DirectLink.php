bc8eea5e2ff31ec98877906b384cf1e906bf7b50
FeeZotter <90189171+FeeZotter@users.noreply.github.com>
2022-10-30 19:02:09 +0100

    removed old directLink code, needs rewriting

--- DirectLink.php
+++ DirectLink.php
@@ -6,54 +6,12 @@ class DirectLink
 {
     public function Person(string $person)
     {
-        //innitialize classes
-        $html = new HTML($person);
-        $htmlComp = new HTMLModules();  
-
-        //html scripts
-        $html->addScript('<script src="./js/index.js"></script>');
-        $html->addScript('<script src="./js/viewLayer1.js"></script>');
-
-        $html->addToHead('<link rel="stylesheet" href="./style/style.css">');
-        $html->addToHead('<link rel="stylesheet" 
-                          href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" 
-                          integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" 
-                          crossorigin="anonymous">');
-
-        $html->addToBody($htmlComp->nextPage('ViewLayer3.php', 'cross_person_categories_id'));
-        
-        $name = $htmlComp->personTable('name', 'persons', 'alias', $person);
-
-        $html->addToBody($htmlComp->categoriesTable('categories_id',
-                                                    'cross_person_categories_id', 
-                                                    'cross_person_categories', 
-                                                    "persons_id='$name'"));
-
-        echo $html->getHTML();
+        //need rewriting, should be like main only that it moves directly to the person
     }
 
     public function Preference(int $cross_person_categories_id)
     {
-        $htmlComp = new HTMLModules();  
-
-        $html = new HTML($htmlComp->getFirstMatchValue('categories_id', 'cross_person_categories', "cross_person_categories_id='$cross_person_categories_id'"));
-
-        //html scripts
-        $html->addScript('<script src="./js/index.js"></script>');
-        $html->addScript('<script src="./js/viewLayer1.js"></script>');
-
-        $html->addToHead('<link rel="stylesheet" href="./style/style.css">');
-        $html->addToHead('<link rel="stylesheet" 
-                          href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" 
-                          integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" 
-                          crossorigin="anonymous">');
-
-        $html->addToBody($htmlComp->preferenceTable('preference',
-                                                    'rating', 
-                                                    "preferences", 
-                                                    "cross_person_categories_id='$cross_person_categories_id'"));
-
-        echo $html->getHTML();
+        //need rewriting, should be like main only that it moves directly to the category of a person
     }
 
     public function main()
