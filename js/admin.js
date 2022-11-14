402971daf4b60c36c52afa130da9cdaec7742844
FeeZotter <90189171+FeeZotter@users.noreply.github.com>
2022-11-14 21:45:12 +0100

    created admin.js with key generation beginnings

---
+++ js/admin.js
@@ -0,0 +1,33 @@
+//////////////////////////////
+//        generateKeys      //
+//////////////////////////////
+
+function getKeys()
+{
+    var jqxhr = $.get(url, function() {
+        //nothing
+    })
+    .done(function(data) {
+        document.getElementById('Keys').innerHTML += ' {Key: ' + '' + ' , uses: ' + '' + ' } ';
+    })
+    .then(function() {
+        createEvents();
+    })
+    .fail(function() {
+        alert( 'request failed' );
+    });
+}
+
+
+//////////////////////////////
+//        copyOnClick       //
+//////////////////////////////
+
+function copyToClipboard(class_) 
+{
+    var copyText = document.getElementsByClassName(class_);
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); // For mobile devices
+    navigator.clipboard.writeText(copyText.value);
+    console.log("Copied the text: " + copyText.value);
+} 
