73bec66c9214270a2d15ad0ee50be756c35195a7
FeeZotter <90189171+FeeZotter@users.noreply.github.com>
2022-11-30 08:14:46 +0100

    fixed critical bug

--- DB.php
+++ DB.php
@@ -6,7 +6,7 @@
 
         private function __construct()
         {
-            include("./config.php");
+            include("config.php");
             self::$conn = new mysqli($hostname, 
                                      $username, 
                                      $password, 
