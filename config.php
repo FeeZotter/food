308f3499a9527fff1597af51f7c902ccef0ec232
FeeZotter <90189171+FeeZotter@users.noreply.github.com>
2022-12-12 11:31:11 +0100

    config change passwd

--- config.php
+++ config.php
@@ -3,10 +3,10 @@
 
     $hostname = "localhost";
     $username = "mypref";
-    $password = "wH5dKtdFaUe3wbX,";
+    $password = "wH5dKtdFaUe3wbX";
     $database = "mypreferences";/**/
-    /*
     
+/*    
     $hostname = "localhost";
     $username = "myprefs";
     $password = "wH5dKtdFaUe3wbX,";
