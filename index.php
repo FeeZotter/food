a21cc7277c38ea64d8a258b32dfd6d950bafaad6
FeeZotter <90189171+FeeZotter@users.noreply.github.com>
2023-02-11 01:25:51 +0100

    stuff is static

--- index.php
+++ index.php
@@ -6,30 +6,25 @@ include('./food/HTML.php');
 // Add base route (startpage)
 Route::add('/',function()
 {
-    $html = new HTML();
-    echo $html->main();
+    echo HTML::main();
 }, 'get');
 
 //only main table
 Route::add('/get',function()
 {
-    $htmlComp = new HTML();  
-    echo $htmlComp->returnTable('alias', 'persons');
+    echo HTML::returnTable('alias', 'persons');
 }, 'get');
 
 //get preference table
 Route::add('/get/([0-9]*)',function($preferenceID)
 {
-    $htmlComp = new HTML();  
-    echo $htmlComp->returnPreferenceTable($preferenceID);
+    echo HTML::returnPreferenceTable($preferenceID);
 }, 'get');
 
 //get person table
 Route::add('/get/([a-z,0-9]*)',function($alias)
 {
-    $htmlComp = new HTML();  
-    $name = $htmlComp->getName($alias);
-    return $htmlComp->returnCategoriesTable($name);
+    return HTML::returnCategoriesTable(HTML::getName($alias));
 }, 'get');
 
 
@@ -45,26 +40,22 @@ Route::add('/newKey',function()
 ///////////user functions/////////////
 Route::add('/login',function()
 {
-    $htmlComp = new HTML();
-    echo $htmlComp->login("", "");
+    echo HTML::login("", "");
 }, 'get');
 
 Route::add('/login',function()
 {
-    $htmlComp = new HTML();
-    echo $htmlComp->userMainPage($_REQUEST['inputName'], $_REQUEST['inputPassword']);
+    echo HTML::userMainPage($_REQUEST['inputName'], $_REQUEST['inputPassword']);
 }, 'post');
 
 Route::add('/login/table',function()
 {
-    $htmlComp = new HTML();
-    echo $htmlComp->onlyUserCategoyTable($_REQUEST['inputName'], $_REQUEST['inputPassword']);    
+    echo HTML::onlyUserCategoyTable($_REQUEST['inputName'], $_REQUEST['inputPassword']);    
 }, 'post' );
 
 Route::add('/regrister',function()
 {
-    $htmlComp = new HTML();
-    echo $htmlComp->regristration();
+    echo HTML::regristration();
 }, 'get');
 
 Route::add('/regrister',function()
@@ -85,8 +76,7 @@ Route::add('/reg',function()
 
 Route::add('/admin',function()
 {
-    $admin = new HTML();
-    echo $admin->adminPage();
+    echo HTML::adminPage();
 }, 'get');
 
 
@@ -95,22 +85,19 @@ Route::add('/admin',function()
 //shortcut to preference ID by id
 Route::add('/s/([0-9]*)',function($id)
 {
-    $preference = new HTML();
-    echo $preference->PreferenceByID($id);
+    echo HTML::PreferenceByID($id);
 }, 'get');
 
 //shortcut to person
 Route::add('/s/([a-z,0-9]*)',function($name)
 {
-    $person = new HTML();
-    echo $person->Person($name);
+    echo HTML::Person($name);
 }, 'get');
 
 //shortcut to preference ID by alias and category
 Route::add('/s/([a-z,0-9]*)/([a-z,0-9]*)',function($alias, $category)
 {
-    $preference = new HTML();
-    echo $preference->Preference($alias, $category);
+    echo HTML::Preference($alias, $category);
 }, 'get');
 
 ////////////////
