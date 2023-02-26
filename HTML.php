e9f38941d4ad0da89e1ceccb2d9c8f7a06e04354
FeeZotter <90189171+FeeZotter@users.noreply.github.com>
2023-02-26 08:45:24 +0100

    footer is buty

--- HTML.php
+++ HTML.php
@@ -51,41 +51,51 @@ class HTML
     {
         return '
         <footer class="bg-light text-center text-lg-start" style="width: 100%;">
-        <div class="container p-4 pb-0">
-            <form action="">
-                <div class="col">
-                    <div class="col-auto mb-4 mb-md-0">
+        <div class="container p-4 pb-0 row row-cols-3 py-3 my-3 border-top">
+            <div class="col">
+                <div class="col-auto mb-4 mb-md-0">
                     <h1>Impressum</h1>
                     <table>
                         <tbody>
                             <tr>
-                                <th scope="col"><b>Name:</b></th>
+                                <th scope="col"><b>Name: </b></th>
                                 <th scope="col">Fee-Zara Julianna Zotter</th>
                             </tr>  
                             <tr>
-                                <th scope="col"><b>E-Mail:</b></th>
-                                <th scope="col">julianazotter@proton.me</th>
+                                <th scope="col"><b>E-Mail: </b></th>
+                                <th scope="col"> julianazotter@proton.me</th>
                             </tr>   
                         </tbody>
                     </table>
-                    </div>
                 </div>
-            </form>
+            </div>
+            <div class="col">
+            </div>
             <div class="col">
                 <div class="col-auto mb-4 mb-md-0">
-                <h1>Links</h1>
-                <table>
-                    <tbody>
-                        <tr>
-                            <th scope="col"><a href="localhost/login">login</a></th>
-                        </tr>  
-                        <tr>
-                            <th scope="col"><b>E-Mail:</b></th>
-                            <th scope="col">julianazotter@proton.me</th>
-                        </tr>   
-                    </tbody>
-                </table>
+                    <h1>Links</h1>
+                    <table>
+                        <tbody>
+                            <tr>    
+                                <th scope="col"><a href="/">main page</a></th>
+                            </tr> 
+                            <tr>
+                                <th scope="col"><a href="/login">login</a></th>
+                            </tr>  
+                            <tr>
+                                <th scope="col"><a href="/reg">regristration (key neened)</a></th>
+                            </tr>   
+                            <tr>
+                                <th scope="col"><a href="/impressum">impressum</a></th>
+                            </tr>  
+                            <tr>
+                                <th scope="col"><a href="/hints">API and shortlink manual (not implemented -> blank page)</a></th>
+                            </tr> 
+                        </tbody>
+                    </table>
                 </div>
+            </div>
+            <div class="col">
             </div>
         </div>
         <br>
