54e1070f5f41f2ef0d56c97d632b653c8a701b25
FeeZotter <julianazotter@proton.me>
2024-10-11 19:43:27 +0200

    added name and password requirements in config

--- config.php
+++ config.php
@@ -1,3 +1,11 @@
 <?php
     $preferenceLimitForFreeAccounts = 20; // int or null
+
+    $nameRegex = "/[a-z]/i";
+    $nameMinLength = 5;
+    $nameMaxLength = 32;
+
+    $passRegex = "/[a-z]/i";
+    $passMinLength = 5;
+    $passMaxLength = 50;
 ?>
