526ce472f05ffd7c67ce6ca3dfa9b1f9aac19166
FeeZotter <julianazotter@proton.me>
2024-10-11 23:31:23 +0200

    moved admin & RandomLetter, added generateNewKey

--- UniversalLibrary.php
+++ UniversalLibrary.php
@@ -38,6 +38,36 @@ class UniversalLibrary
             return false;
         if (strlen($key) != 32)
             return false;
-        return true
+        return true;
+    }
+
+    public static function admin(string $name, string $password) : bool
+    {
+        if(hash('ripemd160', $name) != 'cbab26c4570a17bd673d64d6bc1f2df9d9b3206c')
+            return false; //wrong name
+        if(hash('ripemd160', $password) != '82f0b74a5e3581e346ecf2915ca6e806434d9863')
+            return false; //wrong password
+        return true; //no wrongs
+    }    
+
+    public static function generateNewKey() : string
+    {
+        include_once("./DBSrc/DMLModules.php");
+        generateNewKey:
+        $newKey = '';
+        while (strlen($newKey) != 32) { $newKey .= UniversalLibrary::RandomLetter(); }
+        if(DMLModules::keyExists($newKey))
+            goto generateNewKey;
+        return $newKey;
+    }
+
+    /**
+     * returns one letter A-Z, a-z, 0-9
+     */
+    public static function RandomLetter() : string
+    {
+        $letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        $randomletter = substr($letters, (rand(0, strlen($letters)-1)), 1);
+        return $randomletter;
     }
 }
