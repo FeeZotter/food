f95ef1c7621c4e8c74134448c0b3ab026abf6b89
FeeZotter <julianazotter@proton.me>
2024-10-12 03:51:06 +0200

    fixed impressum

--- Pages/impressum.php
+++ Pages/impressum.php
@@ -1,13 +1,13 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Preferix</title>
+    <title>Preferix - Impressum</title>
     <link rel='stylesheet' href='/food/style/style.css'>
     <link rel='stylesheet' href='/food/style/bootstrap-5.2.2-dist/css/bootstrap.min.css'>
 </head>
 <body>
     <div id='content' style='width:100vw;margin:auto;'>
-        <?php echo(include_once("./modules/impressumText.php"));?>
+        <?php echo(include_once("./Pages/modules/impressumText.php"));?>
     </div>
 </body>
 </html>
