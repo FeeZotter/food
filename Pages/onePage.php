12a15e3b0fb7216444b2012430bf715e72228267
FeeZotter <julianazotter@proton.me>
2024-10-27 12:24:53 +0100

    added empty page/{page}.php

--- Pages/onePage.php
+++ Pages/onePage.php
@@ -1,35 +1,35 @@
 <!DOCTYPE html>
 <head>
     <title>Preferix - TabTest</title>
+    <link rel='stylesheet' href='style\temporary.css'>
     <link rel='stylesheet' href='style\tabs.css'>
     <link rel='stylesheet' href='style\style.css'>
 </head>
 <body>
      <!-- Tab links -->
     <div class="tab">
-        <button class="tablinks" id="homeTabBtn"            onclick="openTab(event, 'Home')"            >Home</button>
-        <button class="tablinks" id="preferenceListTabBtn"  onclick="openTab(event, 'preferenceList')"  >PreferenceList</button>
-        <button class="tablinks" id="preferencesTabBtn"     onclick="openTab(event, 'preferences')"     >Preferences</button>
+        <button class="tablinks todo" id="homeTabBtn"            onclick="openTab(event, 'Home')"            >Home</button>
+        <button class="tablinks todo" id="preferenceListTabBtn"  onclick="openTab(event, 'preferenceList')"  >PreferenceList</button>
+        <button class="tablinks todo" id="preferencesTabBtn"     onclick="openTab(event, 'preferences')"     >Preferences</button>
         <button class="tablinks" id="loginTabBtn"           onclick="openTab(event, 'login')"           >Login</button>
-        <button class="tablinks" id="userSpaceTabBtn"       onclick="openTab(event, 'userSpace')"       >User Space</button>
+        <button class="tablinks todo" id="userSpaceTabBtn"       onclick="openTab(event, 'userSpace')"       >User Space</button>
         <button class="tablinks" id="impressumTabBtn"       onclick="openTab(event, 'impressum')"       >Impressum</button>
-        <button class="tablinks" id="apiTabBtn"             onclick="openTab(event, 'api')"             >API documentation</button>
+        <button class="tablinks todo" id="apiTabBtn"             onclick="openTab(event, 'api')"             >API documentation</button>
     </div>
 
     <!-- Tab content -->
     <div id="Home" class="tabcontent">
-        <h1>TODO</h1>
         <?php include_once("Pages/tabs/home.php") ?>
     </div>
 
     <div id="preferenceList" class="tabcontent">
-        <h1>TODO</h1>
         <h3>PreferenceList</h3>
+        <?php include_once("Pages/tabs/preferenceList.php") ?>
     </div>
 
     <div id="preferences" class="tabcontent">
-        <h1>TODO</h1>
         <h3>preferences</h3>
+        <?php include_once("Pages/tabs/preferences.php") ?>
     </div>
 
     <div id="login" class="tabcontent">
@@ -39,7 +39,6 @@
     </div>
 
     <div id="userSpace" class="tabcontent">
-        <h1>TODO</h1>
         <?php include_once("Pages/tabs/userSpace.php"); ?>
     </div>
 
@@ -48,8 +47,7 @@
     </div> 
 
     <div id="api" class="tabcontent">
-        <h1>TODO</h1>
-        <h3>API STUFF</h3>
+        <?php include_once("Pages/tabs/api.php") ?>
     </div> 
 
     
