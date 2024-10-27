2335320bde4fcc7c9ff38ca9f24eaa1ea4cd250d
FeeZotter <julianazotter@proton.me>
2024-10-27 12:38:38 +0100

    different output in userSpace when logged in

--- Pages/tabs/userSpace.php
+++ Pages/tabs/userSpace.php
@@ -1,4 +1,15 @@
-<?php ?>
-
-<h1>NOT LOGGED IN</h1>
-<button onclick="document.getElementById('loginTabBtn').click();">Login or Regrister</button>
+<?php 
+include_once($_SERVER['DOCUMENT_ROOT'].'/Session.php');
+Session::innit();
+if(Session::isLogin())
+{
+  echo "Logged in";
+}
+else
+{
+    echo '
+        <h1>NOT LOGGED IN</h1>
+        <button onclick="document.getElementById(\'loginTabBtn\').click();">Login or Regrister</button>
+    ';
+}
+?>
